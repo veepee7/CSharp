a890c45c053402f8e95d5bd8aa14b3872dc9b5aa
Ville-Petteri Särmä <sarma.villepetteri@gmail.com>
2021-03-30 15:04:51 +0300

    viimeistelty ja kommentoitu

--- Program.cs
+++ Program.cs
@@ -8,56 +8,36 @@ namespace CSharp
             string syöte;
             int vastaus;
 
+            // Kone arpoo pelin oikean vastauksen
             Random rd = new Random();
             int generatednumber = rd.Next(1,21);
 
             Console.WriteLine("Tervetuloa Arvaa Luku -peliin!");
-            Console.WriteLine("Arvaa lukujen 1-20 väliltä, sinulla on kolme arvausta:");
 
-            syöte = Console.ReadLine();
-            vastaus = Convert.ToInt32(syöte);
+            // Määritetään arvauskertojen määrä
+            for (int arvauskerta = 0; arvauskerta < 3; arvauskerta++)
+            {
+                Console.WriteLine("Arvaa lukujen 1-20 väliltä, sinulla on kolme arvausta:");
+               
+                // Pyydetään syöte ja muutetaan string int-muotoon
+                syöte = Console.ReadLine();
+                vastaus = Convert.ToInt32(syöte);
 
-            if (vastaus < generatednumber) {
-                Console.WriteLine("Oikea vastaus on suurempi kuin vastauksesi.");
+                // Määritetään onko tulos oikea ja tällöin lopettaa pelin. Jos ei, kone pyytää arvaamaan uudestaan tai lopettaa pelin viimeiseen kysymykseen.
+                if (vastaus < generatednumber) {
+                    Console.WriteLine("Oikea vastaus on suurempi kuin vastauksesi.");
+                }
+                else if (vastaus > generatednumber) {
+                    Console.WriteLine("Oikea vastaus on pienempi kuin arvauksesi.");
+                }
+                else {
+                    Console.WriteLine("Arvasit oikein!");
+                    break;
+                }
             }
-            else if (vastaus > generatednumber) {
-                Console.WriteLine("Oikea vastaus on pienempi kuin arvauksesi.");
-            }
-            else {
-                Console.WriteLine("Arvasit oikein!");
-                System.Environment.Exit(1);
-            }
-
-            Console.WriteLine("Arvaa toisen kerran:");
-
-            if (vastaus < generatednumber) {
-                Console.WriteLine("Oikea vastaus on suurempi kuin vastauksesi.");
-            }
-            else if (vastaus > generatednumber) {
-                Console.WriteLine("Oikea vastaus on pienempi kuin arvauksesi.");
-            }
-            else {
-                Console.WriteLine("Arvasit oikein!");
-                System.Environment.Exit(1);
-            }
-
-            Console.WriteLine("Arvaa viimeisen kerran:");
-
-            if (vastaus < generatednumber) {
-                Console.WriteLine("Oikea vastaus on suurempi kuin vastauksesi.");
-            }
-            else if (vastaus > generatednumber) {
-                Console.WriteLine("Oikea vastaus on pienempi kuin arvauksesi.");
-            }
-            else {
-                Console.WriteLine("Arvasit oikein!");
-                System.Environment.Exit(1);
-            }
-            
+            // Peli kertoo oikean vastauksen ja ilmoittaa pelin lopusta.
             Console.WriteLine("Oikea vastaus on " + generatednumber + ".");
-            Console.WriteLine("Peli ohi!");
-
-        System.Environment.Exit(1);
+            Console.WriteLine("Peli ohi.");
         }
     }
 }
